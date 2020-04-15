1b972c5199d58c922b5cec773b83512429119632
Heber Ribeiro <heber.r@outlook.com>
2020-04-15 16:17:04 -0300

    Connection to the database and post method

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const crypto = require('crypto');   // encryption package that comes with the node
 
+const connection = require('./database/connection')
+
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
+routes.post('/users', async (request, response) => {
     const { name, email, whatsapp, city, uf } = request.body;
 
     const id = crypto.randomBytes(4).toString('HEX');   // crypto method that creates random characters
 
-    return response.json();
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    })
+
+    return response.json({ id });
 });
 
 module.exports = routes;
