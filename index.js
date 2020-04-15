066aeb0731b745f659c6073f47ed86d2a7db1c3c
Heber Ribeiro <heber.r@outlook.com>
2020-04-15 13:16:09 -0300

    Testing resources

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const app = express();
 
+
+app.post('/users', (request, response) => {
+    const query = request.query;
+
+    return response.json(query);
+});
+
+app.get('/users/:id', (request, response) => {
+     const par = request.params;
+    
+     return response.json(par);
+});
+
+
 app.listen(3333);
