c21d5bc653e3b291e7dc6e25911a95e089a749fe
Heber Ribeiro <heber.r@outlook.com>
2020-04-15 15:27:16 -0300

    Running create_ongs migration

--- src/database/migrations/20200415135943_create_ongs.js
+++ src/database/migrations/20200415135943_create_ongs.js
@@ -1,8 +1,23 @@
-
+/*
+ * Reference http://knexjs.org/#Migrations
+ * Command to create a migration: npx knex migrate:make name_of_migration
+ * Command to running a migration: npx knex migrate:latest * 
+ */
 exports.up = function(knex) {
-  
+
+    return knex.schema.createTable('ongs', function (table) {
+       table.string('id').primary();
+       table.string('name').notNullable();
+       table.string('email').notNullable();
+       table.string('whatsapp').notNullable();
+       table.string('city').notNullable();
+       table.string('uf', 2).notNullable();       
+    })
+      
 };
 
 exports.down = function(knex) {
-  
+
+  return knex.schema.dropTable("ongs");
+
 };
