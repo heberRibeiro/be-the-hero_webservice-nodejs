26ac868e0bd094e7f1130ee5da0d2b103f0bb4ff
Heber Ribeiro <heber.r@outlook.com>
2020-04-15 13:21:58 -0300

    Code restructuring

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,17 @@
 const express = require('express');
 
-const app = express();
+const routes = express.Router();
 
-
-app.post('/users', (request, response) => {
+routes.post('/users', (request, response) => {
     const query = request.query;
 
     return response.json(query);
 });
 
-app.get('/users/:id', (request, response) => {
+routes.get('/users/:id', (request, response) => {
      const par = request.params;
     
      return response.json(par);
 });
 
-
-app.listen(3333);
+module.exports = routes;
